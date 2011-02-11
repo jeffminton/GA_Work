e651a890dc31243087d5e99e4e23947272e50b85
Jeffrey Minton <jeffrey.minton@gmail.com>
2011-02-11 18:39:47 -0500

    ran tests on values of k in kmeans

--- matlab/findObjects.m
+++ matlab/findObjects.m
@@ -1,10 +1,10 @@
-function [edges] = findObjects(img)
+function [edges sig rgb_segment_mask] = findObjects(img, segments)
 
 %copy input image
 imgBak = img;
 
 %set segments to find
-segments = 5;
+%segments = 5;
 
 %write the original image to disk
 imwrite(img, 'imgout/segOrig.jpg');
@@ -18,14 +18,25 @@ imwrite(gray, 'imgout\segGray.jpg');
 %initialize the edges struct
 edges = segment_masks;
 
+sigImg = cell(1, size(segment_masks, 2));
+sig = cell(1, size(segment_masks, 2));
+
 %perform task on each segment mask returned
 for i = 1:size(segment_masks, 2)
     %copy image to segImg for masking
     segImg = imgBak;
     %turn the segment_mask at i into a 3 dimensional matrix
     rgb_segment_mask = repmat(segment_masks{i}, [1, 1, 3]);
+    %create signature img
+    sigImg{i} = imgBak(rgb_segment_mask ~= 0);
+    %create signature array
+    nrows = size(sigImg{i}, 1);
+    ncols = size(sigImg{i}, 2);
+    size(sigImg{i})
+    %sig{i} = reshape(sigImg{i}, nrows * ncols, 3);
     %set pixels in segImg to 0 where they are 0 in mask
-    segImg(rgb_segment_mask == 0) = 0;
+    %segImg((rgb_segment_mask(:, :, 1) == 0) && (rgb_segment_mask(:, :, 2) == 0) && (rgb_segment_mask(:, :, 3) == 0)) = 0;
+    segImg(rgb_segment_mask(:, :, :) == 0) = 0;
     %write segmented color file to disk
     imwrite(segImg, ['imgout\segColor', num2str(i), '.jpg']);
     %perform edge detection on segment mask i
